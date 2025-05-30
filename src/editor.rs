14130200a67f692ab17a1df5a45bbd61ba117186
fallingdan <onewaylimited@gmail.com>
2025-05-30 10:48:09 -0500

    Adds welcome message

--- src/editor.rs
+++ src/editor.rs
@@ -8,6 +8,9 @@ use crossterm::event::{Event, KeyEvent, KeyModifiers};
 use crossterm::terminal::ClearType;
 use terminal::{Position, Terminal};
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
     startup_complete: bool,
@@ -69,6 +72,7 @@ impl Editor {
 
         if self.should_quit {
             Terminal::clear_screen()?;
+            Terminal::move_cursor_to(Position { x: 0, y: 0 });
             Terminal::print("Goodbye!")?;
         } else {
             Editor::draw_rows()?;
@@ -93,10 +97,27 @@ impl Editor {
         Ok(())
     }
 
+    /// Displays the welcome message to Hecto
+    /// If the terminal width is too small, message will not display
     fn display_welcome() -> Result<(), Error> {
         let size = Terminal::get_size()?;
         let target_row = size.height / 3;
-        dbg!(target_row);
+        let welcome_message = format!("{NAME} -- {VERSION}");
+        let welcome_length = welcome_message.len() as u16;
+
+        if size.width < welcome_length {
+            return Ok(());
+        }
+
+        let message_start_position = (size.width - welcome_length) / 2;
+
+        Terminal::move_cursor_to(Position {
+            x: message_start_position - 1,
+            y: target_row,
+        })?;
+        Terminal::print(&welcome_message)?;
+        Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
+        Terminal::flush()?;
 
         Ok(())
     }
